f28b8d9dff0be85c9c11513a91b815af88c893f1
chris <cgisala@msn.com>
2021-02-03 04:57:23 -0600

    test class is full

--- test_studentlists.py
+++ test_studentlists.py
@@ -117,7 +117,14 @@ class TestStudentLists(TestCase):
         self.assertIsNone(test_class.index_of_student('Student2'))
     
     ## TODO write a test for your new is_class_full method when the class is full. use assertTrue.
-
+    def test_is_class_full(self):
+        test_class = ClassList(5)
+        test_class.add_student('Student1')
+        test_class.add_student('Student2')
+        test_class.add_student('Student3')
+        test_class.add_student('Student4')
+        test_class.add_student('Student5')
+        self.assertTrue(test_class.is_class_full(test_class))
 
     ## TODO write a test for your new is_class_full method for when is empty, and when it is not full. Use assertFalse.
 
